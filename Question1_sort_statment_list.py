d09c79d6899dfea84c688e0cfe4c28e4a1ba2f52
Siphai PHOL <100112002+SIPHAI@users.noreply.github.com>
2022-07-08 15:47:14 +0700

    Add files via upload

---
+++ Question1_sort_statment_list.py
@@ -0,0 +1,6 @@
+l=[12,30,45,43,23,6,49,75,86,4,5,34,46,78,56,35,24,88,66,35,23]
+i=[12,30,45,43,23,6,49,75,86,4,5,34,46,78,56,35,24,88,66,35,23]
+l.sort()
+i.sort(reverse = True)
+print("The sort of the list is :",l)
+print("The reverse of the list :",i)
